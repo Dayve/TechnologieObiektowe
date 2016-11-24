2a3072dd42b4b19c2979999e920e72869e811bfb
BladeRunn3r <tkrzywonos@gmail.com>
2016-11-24 21:47:37 +0100

    Revert "added registration fuctionality"
    This reverts commit f71c15796c991293be7678ecb3192f6597bab4bb.

--- TO_klient/src/sciCon/controller/ApplicationController.java
+++
@@ -1,8 +0,0 @@
-package sciCon.controller;
-import javafx.event.ActionEvent;
-import sciCon.Controllers;
-import sciCon.dbInterface;
-
-public class ApplicationController implements Controllers, dbInterface {
-
-}
